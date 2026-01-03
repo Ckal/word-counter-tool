bfbcefddb54df873acb79d946e9298cc134d91df
Ckal <christof.kaller@gmail.com>
2026-01-03 11:13:10 +0100

    test: word-counter-tool v1.0.1 - testing deployment pipeline

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from transformers.tools.base import launch_gradio_demo
 from word_counter import WordCounterTool
 
+# Version: 1.0.1 - Testing deployment pipeline
 launch_gradio_demo(WordCounterTool)
